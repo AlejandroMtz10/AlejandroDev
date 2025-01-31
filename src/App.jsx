9a2edde8107b69a04f44ea251a84c9a02c277d4b
AlejandroMtz10 <alex10mtz_rivera@outlook.com>
2025-01-30 19:31:07 -0600

    Changes on the dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-/* eslint-disable no-unused-vars */
-import {
-  createRoutesFromElements,
-  createBrowserRouter,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-
+import { HashRouter, Routes, Route } from "react-router-dom";
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import "./App.css";
 import Layout from "./layout/Layout";
@@ -13,21 +7,21 @@ import AboutMe from "./pages/AboutMe/AboutMe";
 import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
-import './assets/fonts/stylesFonts.css'
+import './assets/fonts/stylesFonts.css';
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="AboutMe" element={<AboutMe />} />
-        <Route path="Contact" element={<Contact />} />
-        <Route path="Projects" element={<Projects />} />
-      </Route>
-    )
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="AboutMe" element={<AboutMe />} />
+          <Route path="Contact" element={<Contact />} />
+          <Route path="Projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
-
-  return <RouterProvider router={router} />;
 }
 
 export default App;
