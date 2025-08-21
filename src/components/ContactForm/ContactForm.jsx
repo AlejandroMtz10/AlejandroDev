132de5f8f926ff2a0c050e47dbe26ea92d8d0d9b
AlejandroMtz10 <alex10mtz_rivera@outlook.com>
2025-08-21 14:33:01 -0600

    (Update) I changed the connection with the API, now I use enviroment variables
    (Update) I changed the connection with the API, now I use enviroment variables, I add the file .env with the enviroment variables and I updated the component  ContactForm.jsx to use the enviroment variables

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -46,40 +46,40 @@ const ContactForm = () => {
         setIsLoading(true);
 
         try {
-        const result = await emailjs.send(
-            "outlook_alexdev", // Service ID
-            "contact_form_alexDev", // Template ID
-            {
-            from_email: formData.email,
-            subject: formData.subject,
-            message: formData.message,
-            to_email: "alex10mtz_rivera@outlook.com", // My email address
-            },
-            "Sum2w31VHrduOp0aZ" //API key
-        );
+            const result = await emailjs.send(
+                import.meta.env.VITE_EMAILJS_SERVICE_ID,
+                import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+                {
+                    from_email: formData.email,
+                    subject: formData.subject,
+                    message: formData.message,
+                    to_email: import.meta.env.VITE_RECEIVER_EMAIL,
+                },
+                import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+            );
 
-        console.log("EmailJS result:", result);
+            console.log("EmailJS result:", result);
 
-        toast.success("Email sent successfully!", {
-            position: "bottom-right",
-            theme: "dark",
-            autoClose: 3000,
-        });
+            toast.success("Email sent successfully!", {
+                position: "bottom-right",
+                theme: "dark",
+                autoClose: 3000,
+            });
 
-        setFormData({
-            email: "",
-            subject: "",
-            message: "",
-        });
+            setFormData({
+                email: "",
+                subject: "",
+                message: "",
+            });
         } catch (error) {
-        console.error("EmailJS error:", error);
-        toast.error("Failed to send email. Please try again.", {
-            position: "bottom-right",
-            theme: "dark",
-            autoClose: 3000,
-        });
+            console.error("EmailJS error:", error);
+            toast.error("Failed to send email. Please try again.", {
+                position: "bottom-right",
+                theme: "dark",
+                autoClose: 3000,
+            });
         } finally {
-        setIsLoading(false);
+            setIsLoading(false);
         }
     };
 
